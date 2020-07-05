9dbad6b947435fa6a07c1a32774ee3fbbaac8dc3
Anwesh43 <anweshthecool0@gmail.com>
2020-07-06 05:03:25 +0530

    adding NNService class

--- index.js
+++ index.js
@@ -12,3 +12,25 @@ class TrainingData {
         return new TrainingData(input, output)
     }
 }
+
+class NNService {
+
+    neuralNetwork
+    trainingData = []
+
+    init() {
+          this.neuralNetwork = new brain.NeuralNetwork({})
+    }
+
+    addTrainingData(input, output) {
+        this.trainingData.push(TrainingData.create(input, output))
+    }
+
+    train() {
+        this.neuralNetwork.train(this.trainingData)
+    }
+
+    run(input) {
+        return this.neuralNetwork.run(input)
+    }
+}
