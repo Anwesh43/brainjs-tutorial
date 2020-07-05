a7541b88c6952c4137e952873b099484140ee851
Anwesh43 <anweshthecool0@gmail.com>
2020-07-06 05:15:18 +0530

    training model and running result

--- index.js
+++ index.js
@@ -34,3 +34,31 @@ class NNService {
         return this.neuralNetwork.run(input)
     }
 }
+
+const input = [[0, 0], [0, 1], [1, 0], [1, 1]]
+const output = [[0], [1], [1], [0]]
+
+const target = [0, 0]
+
+const nnService = new NNService()
+nnService.init()
+
+const messageElement = document.getElementById('message')
+
+const interval = setInterval(() => {
+    var msg = ""
+    if (input.length == 0) {
+        messageElement.innerHTML = "training"
+        nnService.train()
+        const result = nnService.run(target)
+        msg = `trained model and ran ${JSON.stringify(target)} for result ${JSON.stringify(result)}`
+        clearInterval(interval)
+    }  else {
+        const currInput = input.splice(0, 1)[0]
+        const currOutput = output.splice(0, 1)[0]
+        nnService.addTrainingData(currInput, currOutput)
+        msg = `adding ${JSON.stringify(currInput)} : ${JSON.stringify(currOutput)} training data`
+
+    }
+    messageElement.innerHTML = msg
+}, 1000)
